dfdf5a9d5d5d926b83b539676239dc686f4740f2
Celio Iyama <celio.iyama@gmail.com>
2021-07-16 22:46:27 -0300

    Angular requisitos - TypeScript fundamentos

---
+++ typescript/app.ts
@@ -0,0 +1,19 @@
+let message: string = "Helpe me, Obi-Wan Kenobi. You're my only hope"
+
+console.log(message)
+
+let episode: number = 4
+//episode = '4'
+console.log("This is episode " + episode)
+episode += 1
+console.log("Next episode is " + episode)
+
+let favoriteDroid: string //quando nao declaramos a variavel ele atribui any
+favoriteDroid = "BB-8" //any variavel pode ter qualquer valor.
+//favoriteDroid = 10
+//favoriteDroid += 10
+console.log("My favorite droid is: " + favoriteDroid);
+
+let favoriteDroid2
+favoriteDroid2 = 10
+console.log('Teste ${favoriteDroid2}' )
---
+++ typescript/dist/app.js
@@ -0,0 +1,15 @@
+var message = "Helpe me, Obi-Wan Kenobi. You're my only hope";
+console.log(message);
+var episode = 4;
+//episode = '4'
+console.log("This is episode " + episode);
+episode += 1;
+console.log("Next episode is " + episode);
+var favoriteDroid; //quando nao declaramos a variavel ele atribui any
+favoriteDroid = "BB-8"; //any variavel pode ter qualquer valor.
+//favoriteDroid = 10
+//favoriteDroid += 10
+console.log("My favorite droid is: " + favoriteDroid);
+var favoriteDroid2;
+favoriteDroid2 = 10;
+console.log('Teste ${favoriteDroid2}');
