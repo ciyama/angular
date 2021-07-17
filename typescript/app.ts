2638a0e62f8156c97dd420f7021608ac244393c1
Celio Iyama <celio.iyama@gmail.com>
2021-07-17 10:33:17 -0300

    Importando ma biblioteca em JS | npm -init | npm npm install --save lodash@4.14

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,6 +1,8 @@
 import {Spacecraft, ContainerShip} from './base-ships'
 import {MillenniumFalcon} from './starfighters'
 
+import * as _ from 'lodash'
+console.log(_.pad("Typescript Exemples", "40", "="))
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace();
