9e7080f73928b3c7cf682bdc06709019bda27d5e
Celio Iyama <celio.iyama@gmail.com>
2021-07-16 23:39:38 -0300

    TypeScript funçoes e arrow - Angular

--- typescript/app.ts
+++ typescript/app.ts
@@ -8,12 +8,32 @@ console.log("This is episode " + episode)
 episode += 1
 console.log("Next episode is " + episode)
 
-let favoriteDroid: string //quando nao declaramos a variavel ele atribui any
+let favoriteDroid //quando nao declaramos a variavel ele atribui any
 favoriteDroid = "BB-8" //any variavel pode ter qualquer valor.
-//favoriteDroid = 10
-//favoriteDroid += 10
+favoriteDroid = 10
+favoriteDroid += 1
 console.log("My favorite droid is: " + favoriteDroid);
 
-let favoriteDroid2
-favoriteDroid2 = 10
+let favoriteDroid2:string
+favoriteDroid2 = '10'
 console.log('Teste ${favoriteDroid2}' )
+
+
+let isEnoughtToBeatMF = function (parsecs: number) : boolean {
+  return parsecs < 12
+}
+
+let distance = 14
+// distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnoughtToBeatMF(distance) ? 'YES': 'NO'}`)
+
+
+let call = (name: string) => console.log(`Do you copy, ${name}`)
+call('R2')
+
+function inc(speed: number, inc: number = 1): number {
+  return speed + inc
+}
+
+console.log(`inc(5, 1) = ${inc(5, 1)}`)
+console.log(`inc(5) = ${inc(5)}`)
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -7,9 +7,23 @@ episode += 1;
 console.log("Next episode is " + episode);
 var favoriteDroid; //quando nao declaramos a variavel ele atribui any
 favoriteDroid = "BB-8"; //any variavel pode ter qualquer valor.
-//favoriteDroid = 10
-//favoriteDroid += 10
+favoriteDroid = 10;
+favoriteDroid += 1;
 console.log("My favorite droid is: " + favoriteDroid);
 var favoriteDroid2;
-favoriteDroid2 = 10;
+favoriteDroid2 = '10';
 console.log('Teste ${favoriteDroid2}');
+var isEnoughtToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+// distance = 11
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon? " + (isEnoughtToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc(5, 1) = " + inc(5, 1));
+console.log("inc(5) = " + inc(5));
